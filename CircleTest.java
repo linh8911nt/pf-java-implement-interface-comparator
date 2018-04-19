9caaf4e45d9cbf110636902663b4b5ca7c568ec5
nguyenthanhlinh <thanhlinh12314@gmail.com>
2018-04-19 15:52:03 +0700

    implement interface comparator

---
+++ CircleTest.java
@@ -0,0 +1,9 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle("white", false, 3);
+        System.out.println(circle);
+    }
+}
---
+++ SquareTest.java
@@ -0,0 +1,9 @@
+public class SquareTest {
+    public static void main(String[] args) {
+        Square square = new Square(5);
+        System.out.println(square);
+
+        square = new Square("black", false, 3);
+        System.out.println(square);
+    }
+}
